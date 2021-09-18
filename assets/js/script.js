50ce572d850ad7f7ad8d9524b1350c29aadc3cfb
fairfax <lovelace.af@gmail.com>
2021-09-18 00:43:43 -0400

    added html with bootstrap. some basic stylings

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+[
+    {
+       question: 'Who invented JavaScript?',
+       answers: [
+           { options: 'Brendan Eich', correct: true },
+           { options: 'Alan Turing', correct: false },
+           { options: 'Ada Lovelace', correct: false },
+           { options: 'Elon Musk', correct: false },
+       ]
+    },
+
+    {
+      question: 'What does boolean mean in JavaScript?',
+      answers: [
+          { options: 'Yes / No', correct: false },
+          { options: 'Right / Wrong', correct: false },
+          { options: 'True / False', correct: true },
+          { options: 'Bad / Good', correct: false },
+      ]
+   },
+
+   {
+    question: 'Which of the following CANNOT be used with the Math object?',
+    answers: [
+        { options: '.round', correct: false },
+        { options: '.ceil', correct: false },
+        { options: '.random', correct: false },
+        { options: '.PIE', correct: true },
+    ]
+ },
+
+ {
+  question: 'Which of the following IS NOT an arithmetic operator?',
+  answers: [
+      { options: '+', correct: false },
+      { options: '--', correct: false },
+      { options: '**', correct: false },
+      { options: '~', correct: true },
+  ]
+},
+]
