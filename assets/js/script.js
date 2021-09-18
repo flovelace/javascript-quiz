f5872467058cdc49cb1ff24ee022296ef570a153
fairfax <lovelace.af@gmail.com>
2021-09-18 18:09:28 -0400

    start initialises quiz. buttons populate but questions do not. Bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,10 +34,43 @@ var index = 0; //for our questions
 var startPage = document.querySelector("#start-page");
 var startButton = document.querySelector("#start-quiz-btn");
 var showQuestions = document.querySelector("#questions-page");
+var CorrectOrWrong = document.querySelector("#correct-or-wrong");
+var populateQuestion = document.getElementsByClassName("populate-questions");
+var startQuestions = questionList[index];
 
 //listeners here
 startButton.addEventListener("click", runQuiz) //runQuiz will be the function to start
 
 function runQuiz() {
-    console.log('started')
+    console.log('started') //starts the game. confirmed listener working
+    startPage.querySelector("#start-page")
+    startPage.setAttribute("class", "hide");
+    showQuestions.setAttribute("class", "");
+
+    fetchQuestions();
 }
+
+var fetchQuestions = function() {
+    document.getElementsByClassName("populate-questions");
+    populateQuestion.textContent = startQuestions.question;
+    startQuestions.innerHTML = "";
+
+    for (var i = 0; i < 4; i++) {
+        //populates the answers in the buttons
+        var populateList = document.createElement("li");
+        var populateButton = document.createElement("button");
+        populateButton.setAttribute("class", "btn");
+        populateButton.setAttribute("value", startQuestions.answers[i]);
+        populateButton.addEventListener ("click", checkAnswer);
+        populateButton.textContent = i + 1 + ". " + startQuestions.answers[i];
+        populateButton.addEventListener ("click", checkAnswer);
+        populateList.appendChild(populateButton);
+        showQuestions.appendChild(populateList);
+    
+    };
+}
+
+var checkAnswer = function() {
+    
+}
+
