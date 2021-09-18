7a3f96a7ec32c003a17c5383a1147d0c8b703f88
fairfax <lovelace.af@gmail.com>
2021-09-18 01:19:42 -0400

    added small changes to css. added questions to js and began planning out functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,43 @@
-[
+//list of questions
+
+var questionList = [
     {
-       question: 'Who invented JavaScript?',
-       answers: [
-           { options: 'Brendan Eich', correct: true },
-           { options: 'Alan Turing', correct: false },
-           { options: 'Ada Lovelace', correct: false },
-           { options: 'Elon Musk', correct: false },
-       ]
+      question: "Who invented JavaScript?",
+      answers: ["Alan Turing", "Ada Lovelace", "Elon Musk", "Brendan Eich"],
+      correctAnswer: "Brendan Eich"  
     },
 
     {
-      question: 'What does boolean mean in JavaScript?',
-      answers: [
-          { options: 'Yes / No', correct: false },
-          { options: 'Right / Wrong', correct: false },
-          { options: 'True / False', correct: true },
-          { options: 'Bad / Good', correct: false },
-      ]
-   },
+        question: "What does boolean mean in JavaScript?",
+        answers: ["Yes / No", "Right / Wrong", "True / False", "Bad / Good"],
+        correctAnswer: "True / False"  
+    },
 
-   {
-    question: 'Which of the following CANNOT be used with the Math object?',
-    answers: [
-        { options: '.round', correct: false },
-        { options: '.ceil', correct: false },
-        { options: '.random', correct: false },
-        { options: '.PIE', correct: true },
-    ]
- },
+    {
+        question: "Which of the following cannot be used with the Math object?",
+        answers: [".round", ".ceil", ".random", ".PIE"],
+        correctAnswer: ".PIE"  
+    },
 
- {
-  question: 'Which of the following IS NOT an arithmetic operator?',
-  answers: [
-      { options: '+', correct: false },
-      { options: '--', correct: false },
-      { options: '**', correct: false },
-      { options: '~', correct: true },
-  ]
-},
+    {
+        question: "Which of the following is not an arithmetic operator?",
+        answers: ["+", "--", "**", "~"],
+        correctAnswer: "~"  
+    },
 ]
+
+//course stipulates that five questions are required. Will need to add another question
+
+//variables querySelectors
+
+var index = 0; //for our questions
+var startPage = document.querySelector("#start-page");
+var startButton = document.querySelector("#start-quiz-btn");
+var showQuestions = document.querySelector("#questions-page");
+
+//listeners here
+startButton.addEventListener("click", runQuiz) //runQuiz will be the function to start
+
+function runQuiz() {
+    console.log('started')
+}
